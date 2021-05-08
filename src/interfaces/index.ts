faf859c4fb8477e1a7aa5f49a8e3e1ec633e4e15
Leandro <leandrofernandesvieira@gmail.com>
2021-05-07 22:30:29 -0300

    feat: setup interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,45 @@
+export interface Behaviour {
+  id: string;
+  description: string;
+}
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  password_hash: string;
+  created_at: Date;
+  updated_at: Date;
+  id_behaviour: string;
+}
+
+export interface Status {
+  id: string;
+  description: string;
+}
+
+export interface List {
+  id: string;
+  user_id: string;
+  status_id: string;
+}
+
+export interface Product {
+  id: string;
+  name: string;
+}
+
+export interface Unity {
+  id: string;
+  description: string;
+}
+
+export interface ProductsQuantity {
+  id: string;
+  list_id: string;
+  product_id: string;
+  initial_quantity: number;
+  final_quantity?: number;
+  suggestion_quantity?: number;
+  id_unity: string;
+}
