7b77cdf263333264a2b419f9404e5ed0e6c9ec42
Leandro <leandrofernandesvieira@gmail.com>
2021-05-12 20:15:13 -0300

    feat: create loader

--- src/pages/EditList.tsx
+++ src/pages/EditList.tsx
@@ -13,54 +13,26 @@ import { ScrollView } from "react-native-gesture-handler";
 import { CreateFirstList } from "./CreateFirstList";
 import { ListWithStatus } from "../entities/ListWithStatus";
 import { CardProducts } from "../components/CardProducts";
+import { Loader } from "../components/Loader";
+import colors from "../styles/colors";
 
 interface EditListParams {
   url: string;
 }
-
-/* const products: Product[] = [
-  {
-    id: "0",
-    name: "Banana",
-  },
-  {
-    id: "1",
-    name: "Adoçante",
-  },
-  {
-    id: "2",
-    name: "Açúcar",
-  },
-  {
-    id: "3",
-    name: "Papel Higiênico",
-  },
-]; */
 
 let listWithSt: ListWithStatus;
 
 export function EditList() {
   const routes = useRoute();
 
-  const [loading, setLoading] = useState(false);
-  // const [nfResponse, setNfResponse] = useState<ListScrap>();
-
-  //const listWithStatus:ListWithStatus={} as ListWithStatus;
+  const [isLoading, setIsLoading] = useState(true);
 
   // const { url } = routes.params as EditListParams;
   const url =
     "https://www.nfce.fazenda.sp.gov.br/qrcode?p=35210560479680001090651050001600861259534072|2|1|1|643A34EFA0FBBF88AC6EFBB323D294586190ACAF";
 
-  // Only for testing purposes
-  // if (!url) {
-  //   url = "";
-  // }
-  //listWithStatus.status=StatusEnum.pending;
-
   useEffect(() => {
     async function fetchScrappedProducts() {
-      setLoading(true);
-
       try {
         const response = await api.post("/scrap", {
           url_nfce: url,
@@ -73,13 +45,17 @@ export function EditList() {
         // listWithSt.listProdScrap = nfResponse as ListScrap;
       } catch (error) {
         console.log(error);
-      } finally {
-        setLoading(false);
       }
+
+      setIsLoading(false);
     }
 
     fetchScrappedProducts();
   }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
 
   return (
     <ScrollView style={styles.container}>
@@ -89,35 +65,20 @@ export function EditList() {
       <View style={styles.lists}></View>
 
       <View style={styles.lists}>
-        {loading ? (
-          <Text>Carregando...</Text>
-        ) : (
-          <Text>Já carregado!</Text>
-          // nfResponse?.products.map((prod) => (
-          //   <CardProducts product={prod} key={prod.code} />
-          // ))
-        )}
+        <Text>Já carregado!</Text>
+        {/* {nfResponse?.products.map((prod) => (
+          <CardProducts product={prod} key={prod.code} />
+        /))} */}
       </View>
     </ScrollView>
   );
-
-  /* return loading ? (
-    <View style={styles.container}>
-      <Text>Carregando...</Text>
-    </View>
-  ) : (
-    <View style={styles.container}>
-      <ListLayout />
-    </View>
-  ); */
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //alignItems: "center",
-    //justifyContent: "center",
-    paddingHorizontal: 30,
+    paddingHorizontal: 32,
+    backgroundColor: colors.white,
   },
   lists: {
     flex: 1,
